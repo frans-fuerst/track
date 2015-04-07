5d9d34286a8c95140a9bf777136cb0f12752cfd4
frans <frans@scitics.de>
2015-04-07 08:12:41 +0200

    added untested windows code for idle secs and window titles

---
+++ desktop_usage_info/backend_windows/applicationinfo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import logging
+from win32gui import GetWindowText, GetForegroundWindow
+
+
+class UncriticalException(Exception):
+    pass
+
+
+def get_active_window_title():
+    return GetWindowText(GetForegroundWindow())
+
+
+def get_active_process_name():
+    return ""
+
+
+if __name__ == '__main__':
+    import time
+    import sys
+    print(sys.version)
+    while True:
+        print(get_idle_duration())
+        print(get_window())
+        time.sleep(1)
+

3c5a6c025457f97043e9b4b5fa7b9c53d705e936
frans <frans@scitics.de>
2015-09-23 08:40:47 +0200

    added new qt free active_applications class and repaired tests

--- track_qt/__init__.py
+++ track_qt/__init__.py
@@ -1,10 +1,11 @@
 from time_tracker_qt import time_tracker_qt
-#from qt_common import matrix_table_model
-from active_applications import active_applications_qtmodel
+from active_applications_qtmodel import active_applications_qtmodel
 from rules_model import rules_model
 from qt_common import change_emitter
 from timegraph import timegraph
 from qt_common import matrix_table_model
+
+# timegraph is not here because UI import issues
 #from timegraph import timegraph
 
 

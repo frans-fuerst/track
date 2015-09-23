3acac26ef1a101573da00d21d127c5fb328d844d
frans <frans@scitics.de>
2015-09-23 08:41:01 +0200

    add minimalistic command line interface for testing the server

---
+++ track-cli.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import sys
+import zmq
+import logging
+
+log = logging.getLogger('track_cli')
+
+def print_info():
+    log.info("zeromq version: %s" % zmq.zmq_version())
+    log.info("pyzmq version:  %s" % zmq.pyzmq_version())
+
+def send_request(request):
+    context = zmq.Context()
+    req_socket = context.socket(zmq.REQ)
+
+    req_socket.connect('tcp://127.0.0.1:3456')
+
+    req_socket.send_json(request)
+    return req_socket.recv_json()
+
+def handle_result(result):
+    if 'type' in result and result['type'] == 'error':
+        raise Exception('server replied with error: "%s"' % result['what'])
+    print(result)
+    
+def main():
+    args = sys.argv[1:]
+
+    if args == ['quit']:
+        request = {'type': 'quit'}
+    elif args == ['info']:
+        request = {'type': 'info'}
+    else:
+        raise Exception('command not handled: %s' % args)
+    
+    try:
+        result = send_request(request)
+        handle_result(result)
+    except zmq.ZMQError as e:
+        log.error(e)
+        return
+    except KeyboardInterrupt:
+        log.info("got keyboard interrupt - exit")
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()

f4110b0bde29ae0fb3650d1993ce4237eeacfbf0
Frans Fuerst <frans.fuerst@toptica.com>
2015-09-23 09:08:31 +0200

    fix dependency issue

--- track_qt/__init__.py
+++ track_qt/__init__.py
@@ -1,9 +1,9 @@
 from time_tracker_qt import time_tracker_qt
+from qt_common import matrix_table_model
 from active_applications_qtmodel import active_applications_qtmodel
 from rules_model import rules_model
 from qt_common import change_emitter
 from timegraph import timegraph
-from qt_common import matrix_table_model
 
 # timegraph is not here because UI import issues
 #from timegraph import timegraph

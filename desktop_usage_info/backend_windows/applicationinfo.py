c92cc92d3f598eeeac5ab5f5ad08698ba00829c1
frans <frans.fuerst+git@gmail.com>
2017-03-03 20:02:10 +0100

    remove python3 syntax elements - PyQt5 is still hard to get for Python3

--- desktop_usage_info/backend_windows/applicationinfo.py
+++ desktop_usage_info/backend_windows/applicationinfo.py
@@ -3,13 +3,13 @@
 
 from win32gui import GetWindowText, GetForegroundWindow
 
-def _get_active_window_title() -> str:
+def _get_active_window_title():
     return GetWindowText(GetForegroundWindow())
 
-def _get_active_process_name() -> str:
+def _get_active_process_name():
     return ""
 
-def get_active_window_information() -> dict:
+def get_active_window_information():
     return {
         "TITLE": _get_active_window_title(),
         # "PID": ???
